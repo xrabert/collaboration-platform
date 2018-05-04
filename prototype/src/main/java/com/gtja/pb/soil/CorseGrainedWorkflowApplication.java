3fe54650c7fcfa1faf79ac4f83e3cec66d88eabb
xut <citygreen@gmail.com>
2018-05-04 15:25:13 +0800

    Initialization

---
+++ prototype/src/main/java/com/gtja/pb/soil/CorseGrainedWorkflowApplication.java
@@ -0,0 +1,37 @@
+package com.gtja.pb.soil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+/*(exclude = {UserDetailsServiceAutoConfiguration.class })*/
+@SpringBootApplication
+public class CorseGrainedWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorseGrainedWorkflowApplication.class, args);
+	}
+
+	@Configuration
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                .httpBasic().and()
+                .authorizeRequests()
+                    .antMatchers("/index.html", "/", "/home", "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+            // @formatter:on
+        }
+    }
+
+}
---
+++ prototype/src/test/java/com/gtja/pb/soil/CorseGrainedWorkflowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gtja.pb.soil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CorseGrainedWorkflowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
